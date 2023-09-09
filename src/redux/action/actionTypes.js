78e719d5a8fb8f7434e390ef9b20d32387c19351
abeer117 <nabeer161117@bscse.uiu.ac.bd>
2023-09-09 21:44:00 +0600

    styled cursor pointer to anchor tags and objectify action types

--- src/redux/action/actionTypes.js
+++ src/redux/action/actionTypes.js
@@ -1,5 +1,6 @@
-export const TOGGLE_CART = 'TOGGLE_CART';
-export const ADD_TO_CART = 'ADD_TO_CART';
-export const REMOVE_ITEM = 'REMOVE_ITEM';
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
+// all the action types for shopping cart
+export const TOGGLE_CART = "TOGGLE_CART";
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_ITEM = "REMOVE_ITEM";
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
