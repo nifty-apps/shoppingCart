d38e3a7ac4e98c55571ebe1f8c328367b5b909cd
Pavel Rahman Taukir <paveltauker@gmail.com>
2023-09-10 05:38:32 +0600

    Cart Component Update

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,19 +2,13 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { incrItem, decrItem, removeItem, toggleCart } from '../redux/action';
 
-
 const Cart = () => {
-
+    // Use object destructuring to directly extract the needed properties from state
     const { isCartOpen, cart } = useSelector(state => state.cartReducer);
     const dispatch = useDispatch();
 
-
-    const cartTotal = cart.map(item => {
-        return item.price * item.quantity;
-    }).reduce((preVal, curVal) => {
-        return preVal + curVal;
-    }, 0);
-
+    // Calculate the cart total using reduce
+    const cartTotal = cart.reduce((total, item) => total + item.price * item.quantity, 0);
 
     if (isCartOpen) {
         return (
@@ -33,48 +27,44 @@ const Cart = () => {
                         </div>
 
                         <div className="cart_body">
-                            {
+                            {cart.map(item => {
+                                const { id, img, title, price, quantity } = item;
 
-                                cart.map(item => {
-                                    const { id, img, title, price, quantity } = item;
+                                return (
+                                    <div className="cart_items" key={id}>
+                                        <figure className="cart_items_img">
+                                            <img src={img} alt="product-img" />
+                                        </figure>
 
-                                    return (
-                                        <div className="cart_items" key={id}>
-                                            <figure className="cart_items_img">
-                                                <img src={img} alt="product-img" />
-                                            </figure>
-
-                                            <div className="cart_items_info">
-                                                <h4>{title}</h4>
-                                                <h3 className="price">tk. {price.toLocaleString()}</h3>
-                                            </div>
-
-                                            <div className="cart_items_quantity">
-                                                <span
-                                                    onClick={() => dispatch(decrItem(id))}
-                                                >
-                                                    &#8722;
-                                                </span>
-                                                <b>{quantity}</b>
-                                                <span
-                                                    onClick={() => dispatch(incrItem(id))}
-                                                >
-                                                    &#43;
-                                                </span>
-                                            </div>
-
-                                            <div
-                                                title="Remove Item"
-                                                className="cart_items_delete"
-                                                onClick={() => dispatch(removeItem(id))}
-                                            >
-                                                <span>&times;</span>
-                                            </div>
+                                        <div className="cart_items_info">
+                                            <h4>{title}</h4>
+                                            <h3 className="price">tk. {price.toLocaleString()}</h3>
                                         </div>
 
-                                    );
-                                })
-                            }
+                                        <div className="cart_items_quantity">
+                                            <span
+                                                onClick={() => dispatch(decrItem(id))}
+                                            >
+                                                &#8722;
+                                            </span>
+                                            <b>{quantity}</b>
+                                            <span
+                                                onClick={() => dispatch(incrItem(id))}
+                                            >
+                                                &#43;
+                                            </span>
+                                        </div>
+
+                                        <div
+                                            title="Remove Item"
+                                            className="cart_items_delete"
+                                            onClick={() => dispatch(removeItem(id))}
+                                        >
+                                            <span>&times;</span>
+                                        </div>
+                                    </div>
+                                );
+                            })}
                         </div>
 
                         <div className="cart_foot">
@@ -96,8 +86,8 @@ const Cart = () => {
             </>
         );
     }
-
-
+    // Return null if the cart is not open (optional)
+    return null;
 };
 
 export default Cart;
