634564f48400c55525102094262a6f884ab2b029
Pavel Rahman Taukir <paveltauker@gmail.com>
2023-09-10 05:47:19 +0600

    ProductsCard Component Update

--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -2,31 +2,33 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../redux/action';
 
-
-const ProductsCard = (props) => {
-
-    const { img, rating, title, price } = props;
+const ProductsCard = ({ img, rating, title, price }) => {
+    // Destructure the props directly in the function parameter
 
     const dispatch = useDispatch();
 
+    // Handle adding the product to the cart
+    const handleAddToCart = () => {
+        const product = { img, rating, title, price };
+        dispatch(addToCart(product));
+    };
+
     return (
-        <>
-            <div className="product_card">
-                <figure>
-                    <img src={img} alt="item-img" />
-                </figure>
-                <strong className="rating">{rating}</strong>
-                <h4 className="title">{title}</h4>
-                <h3 className="price">tk. {price.toLocaleString()}</h3>
-                <button
-                    type="button"
-                    className="btn"
-                    onClick={() => dispatch(addToCart({ ...props }))}
-                >
-                    Add to cart
-                </button>
-            </div>
-        </>
+        <div className="product_card">
+            <figure>
+                <img src={img} alt="item-img" />
+            </figure>
+            <strong className="rating">{rating}</strong>
+            <h4 className="title">{title}</h4>
+            <h3 className="price">tk. {price.toLocaleString()}</h3>
+            <button
+                type="button"
+                className="btn"
+                onClick={handleAddToCart}
+            >
+                Add to cart
+            </button>
+        </div>
     );
 };
 
