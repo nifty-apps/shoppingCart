26a938606382a9e519023207130c7bdaf906acee
Riya Moni <115640457+shammi-riya@users.noreply.github.com>
2023-09-10 19:37:02 +0600

    add shorting to price and title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import productsData from '../data/productsData';
 import ProductsCard from '../components/ProductsCard';
 
 const Home = () => {
+    const [sortBy, setSortBy] = useState('');
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const handleSortChange = (sortOption) => {
+
+        setSortBy(sortOption);
+        
+    };
+    
+
+    // Apply sorting based on selected option
+    let sortedProducts = [...productsData];
+    if (sortBy === 'priceLowToHigh') {
+        sortedProducts.sort((a, b) => a.price - b.price);
+    } else if (sortBy === 'priceHighToLow') {
+        sortedProducts.sort((a, b) => b.price - a.price);
+    }else if(sortBy === 'nameZToA'){
+       sortedProducts.sort((a,b)=>a.title.localeCompare(b.title))
+    }
+    else if(sortBy === 'nameAToZ'){
+       sortedProducts.sort((a,b)=>b.title.localeCompare(a.title))
+    }
+
     return (
         <>
+
             <section id="home">
                 <div className="container">
+                    <div className="dropdown">
+                        <button className="btn btn-secondary" onClick={() => setShowDropdown(!showDropdown)}>
+                            Sort By <span><img src="/images/down-arrow-5-svgrepo-com.svg" alt="" /></span>
+                        </button>
+                        {showDropdown && (
+                            <div className="dropdown-menu">
+                                <button className="dropdown-item" onClick={() => handleSortChange('priceLowToHigh ')}>Price high to low</button>
+                                <button className="dropdown-item" onClick={() => handleSortChange('priceLowToHigh')}>Price low to High</button>
+                                <button className="dropdown-item" onClick={() => handleSortChange('nameZToA')}>Title Z to A</button>
+                                <button className="dropdown-item" onClick={() => handleSortChange('nameAToZ')}>Title A to Z</button>
+                            </div>
+                        )}
+                    </div>
                     <div className="home_content">
                         {
-                            productsData.map((item) => (
+                            sortedProducts.map((item) => (
                                 <ProductsCard key={item.id} {...item} />
                             ))
                         }
